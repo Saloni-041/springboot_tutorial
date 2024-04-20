e8e49dc9d64bfd22eac53619a968cad3732b367f
Saloni-041 <salonibajaj0411@gmail.com>
2024-04-20 12:45:45 +0530

    Corrected some files

--- src/main/java/com/project/SpringBootApp/exceptions/CustomerAlreadyExistsException.java
+++ src/main/java/com/project/SpringBootApp/exceptions/CustomerAlreadyExistsException.java
@@ -1,5 +1,6 @@
 package com.project.SpringBootApp.exceptions;
 
+//This exception can be thrown when the user tries to add a customer that already exists in the database
 public class CustomerAlreadyExistsException extends RuntimeException{
     private String message;
     public CustomerAlreadyExistsException(String msg)
